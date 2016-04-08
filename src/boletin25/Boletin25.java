449728b97721d1666eac4f9df049f51a70c4b526
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-04-08 12:08:43 +0200

    Boletin 25

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio1 boton = new Ejercicio1();
+        boton.setVisible(true);
+        Ejercicio2 botones = new Ejercicio2();
+        botones.setVisible(true);
+        Ejercicio3 hola = new Ejercicio3();
+        hola.setVisible(true);
+        Ventana1 elbicho = new Ventana1();
+        elbicho.setVisible(true);
+        
+    }
+
+}
